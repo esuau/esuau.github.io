4c145a0a93dc6bbf4180ac866c5f7a3f06c05502
esuau <evan.suau@etu.u-pec.fr>
2019-01-06 16:23:23 +0100

    chore(deps): Upgrade Clarity to version 1.0.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { ClarityModule } from '@clr/angular';
 
@@ -13,6 +14,7 @@ import { BioComponent } from './components/bio/bio.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     ClarityModule
   ],
