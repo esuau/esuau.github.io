85e9edd17bf98c51dcf5776b2b4cf3bbb3980dc9
Evan Suau <evan.suau@outlook.com>
2020-04-22 00:00:48 -0400

    feat(bio): Use labels instead of badges (#23)

--- src/app/components/bio/bio.component.spec.ts
+++ src/app/components/bio/bio.component.spec.ts
@@ -1,5 +1,6 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { BioComponent } from './bio.component';
 
@@ -28,5 +29,10 @@ describe('BioComponent', () => {
   it('shoud include bio-content component', () => {
     const bioContent = fixture.debugElement.nativeElement.querySelector('app-bio-content');
     expect(bioContent).not.toBeNull();
+  });
+
+  it('shoud have clickable labels', () => {
+    const labels = fixture.debugElement.queryAll(By.css('.label.clickable'));
+    expect(labels.length).toEqual(5);
   });
 });
